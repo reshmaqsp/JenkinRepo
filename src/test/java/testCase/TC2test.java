99fcb130e004f05e381abff06544d30a8125af86
USER <USER@DESKTOP-KP39UUC>
2023-06-15 11:14:14 +0530

    AT1 first Commit

---
+++ src/test/java/testCase/TC2test.java
@@ -0,0 +1,14 @@
+package testCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC2test {
+
+	@Test
+	public void m1() {
+		Reporter.log("TC2 passed",true);
+		
+	}
+
+}
